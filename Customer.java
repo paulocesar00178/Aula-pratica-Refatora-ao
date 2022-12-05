7e4ecabd8d78ca7dc4574048d7711fbe5a8d6350
paulo cesar <87917461+paulocesar00178@users.noreply.github.com>
2022-12-05 20:50:17 -0300

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,52 @@
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+   private String _name;
+   private Vector _rentals = new Vector();
+
+   public Customer (String name){
+      _name = name;
+   }
+
+   public void addRental(Rental arg) {
+      _rentals.addElement(arg);
+   }
+   
+   public String getName (){
+      return _name;
+   }
+  
+   public String statement() {
+      return new TextStatement().value(this);
+   }
+      
+   public double getTotalCharge() {
+      double result = 0;
+      Enumeration rentals = _rentals.elements();
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         result += each.getDaysRented();
+         }
+         return result;
+   }
+
+   public int getTotalFrequentRenterPoints(){
+      int result = 0;
+      Enumeration rentals = _rentals.elements();
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         result += each.getFrequentRenterPoints();
+      }
+      return result;
+   }
+
+   public String htmlStatement() {
+      return new HtmlStatement().value(this);
+   }
+   
+   public Enumeration getRentals() {
+	   return _rentals.elements();
+   }
+
+}
---
+++ Rental.java
@@ -0,0 +1,27 @@
+public class Rental {
+
+    Movie _movie;
+    private int _daysRented;
+ 
+    public Rental(Movie movie, int daysRented) {
+       _movie = movie;
+       _daysRented = daysRented;
+    }
+ 
+    public int getDaysRented() {
+       return _daysRented;
+    }
+ 
+    public Movie getMovie() {
+       return _movie;
+    }
+
+    public double getCharge(){
+      return _movie._price.getCharge(_daysRented);
+   }
+
+   public int getFrequentRenterPoints(){
+      return _movie._price.getFrequentRenterPoints(_daysRented);
+   }
+
+ }
